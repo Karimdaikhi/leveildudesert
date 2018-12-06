f6e0b1bee5ae52559ff4a2939683ffef0b17ff48
Stanislas Grobien <sgrobien0@gmail.com>
2018-12-06 17:43:20 +0100

    added api and diagnostic proccess

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
-  resources :products, only: [:index, :show]
-  # resources :profiles, only: [:new]
-  get 'diagnostic', to: 'profiles#diagnostic'
-  get 'results', to: 'profiles#results'
+  resources :products, only: [:show, :update]
+  post '/profile_photo', to: 'profiles#profile_photo'
+  get '/diagnostic', to: 'profiles#diagnostic', as: :diagnostic
+  patch '/profile_complete', to: 'profiles#update'
+  get '/results', to: 'profiles#results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
