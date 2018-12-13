1b5a0b485a0160dff09110613501269511266546
emmanuelle nini <emmanuelle.nini@gmail.com>
2018-12-13 17:01:56 +0100

    order

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_105942) do
+ActiveRecord::Schema.define(version: 2018_12_13_143238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "firstname"
+    t.string "adress"
+    t.string "city"
+    t.string "zipcode"
+    t.text "comment"
+    t.string "email"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -53,4 +67,5 @@ ActiveRecord::Schema.define(version: 2018_12_07_105942) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "products"
 end
